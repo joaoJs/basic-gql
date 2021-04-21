9b59f1ff46642244db267acc101628b8540a7e45
joaoJs <=>
2021-04-21 09:49:27 -0400

    puts graphql and rest server on same file

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const http = require('http');
 
 require('dotenv').config();
 
 const app = express();
+
+// types query / mutation / subscription
+const typeDefs = `
+    type Query {
+        totalPosts: Int!
+    }
+`
+
+// resolvers 
+const resolvers = {
+    Query: {
+        totalPosts: () => 42
+    }
+}
+
+// gql server
+const apolloServer = new ApolloServer({
+    typeDefs, 
+    resolvers
+})
+
+// applyMiddleware method connects ApolloServer to a specific HTTP framework ie: express
+apolloServer.applyMiddleware({ app })
+
+const httpserver = http.createServer(app);
 
 app.get('/rest', (req, res) => {
     res.json({
@@ -10,5 +37,7 @@ app.get('/rest', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT, () => 
-    console.log(`server listening at port ${process.env.PORT}`));
+app.listen(process.env.PORT, () => {
+    console.log(`server listening at port ${process.env.PORT}`);
+    console.log(`gql server listening at port ${process.env.PORT}${apolloServer.graphqlPath}`);
+});
