38785bc3f26fd31437893247dcd094ac7fcce90a
joaoJs <=>
2021-04-22 08:28:49 -0400

    add post model

---
+++ models/Post.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+  }
+});
+
+const Post = model('Post', PostSchema);
+
+module.exports = Post;
